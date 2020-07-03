4510600ea2aefb5f7a7ef12124fc841349937bd0
Vladislav Vyskvarko <vyskvarko95@yandex.ru>
2020-07-03 13:36:25 +0300

    Add Splash screen

--- Fifteen/App.xaml.cs
+++ Fifteen/App.xaml.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Fifteen
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        System.Threading.Mutex mutex;
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            bool createdNew;
+            string mutName = "Application";
+            mutex = new System.Threading.Mutex(true, mutName, out createdNew);
+            if (!createdNew)
+            {
+                MessageBox.Show("Application is already running!");
+                this.Shutdown();
+            }
+        }
     }
 }
